43419a7097413edc4bf0c54f0cbb60a5340b2511
Markiian Yuskevych <markiyan.yuskevych@gmail.com>
2017-12-13 16:28:17 +0200

    added tests

---
+++ src/test/scala/MainTest.scala
@@ -0,0 +1,138 @@
+package com.epam.hubd.spark.scala.core.homework
+
+import java.io.File
+import java.nio.file.Files
+
+import com.epam.hubd.spark.scala.core.util.RddComparator
+import main.scala.Main.{AGGREGATED_DIR, ERRONEOUS_DIR}
+import com.holdenkarau.spark.testing.{RDDComparisons, SharedSparkContext, SparkContextProvider}
+import main.scala.Main
+import org.apache.hadoop.fs.Path
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+import org.junit._
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSuite}
+
+
+class MainTest extends FunSuite with SharedSparkContext with RDDComparisons with BeforeAndAfter
+  with BeforeAndAfterAll with SparkContextProvider {
+
+  override def conf = new SparkConf().setMaster("local").setAppName("motels-home-recommendation test")
+
+  val INPUT_BIDS_SAMPLE = "src/test/resources/bids_sample.txt"
+
+  val INPUT_BIDS_INTEGRATION = "src/test/resources/integration/input/bids.txt"
+  val INPUT_EXCHANGE_RATES_INTEGRATION = "src/test/resources/integration/input/exchange_rate.txt"
+  val INPUT_MOTELS_INTEGRATION = "src/test/resources/integration/input/motels.txt"
+
+  val EXPECTED_AGGREGATED_INTEGRATION = "src/test/resources/integration/expected_output/aggregated"
+  val EXPECTED_ERRORS_INTEGRATION = "src/test/resources/integration/expected_output/error_records"
+
+  private var outputFolder: File = null
+
+  before {
+    outputFolder = Files.createTempDirectory("output").toFile
+  }
+
+  test("should read raw bids") {
+    val expected = sc.parallelize(
+      Seq(
+        List("0000002", "15-04-08-2016", "0.89", "0.92", "1.32", "2.07", "", "1.35"),
+        List("0000001", "06-05-02-2016", "ERROR_NO_BIDS_FOR_HOTEL")
+      )
+    )
+
+    val rawBids = Main.getRawBids(sc, INPUT_BIDS_SAMPLE)
+
+    assertRDDEquals(expected, rawBids)
+  }
+
+  test("should collect erroneous records") {
+    val rawBids = sc.parallelize(
+      Seq(
+        List("1", "06-05-02-2016", "ERROR_1"),
+        List("2", "15-04-08-2016", "0.89"),
+        List("3", "07-05-02-2016", "ERROR_2"),
+        List("4", "06-05-02-2016", "ERROR_1"),
+        List("5", "06-05-02-2016", "ERROR_2")
+      )
+    )
+
+    val expected = sc.parallelize(
+      Seq(
+        "06-05-02-2016,ERROR_1,2",
+        "06-05-02-2016,ERROR_2,1",
+        "07-05-02-2016,ERROR_2,1"
+      )
+    )
+
+    val erroneousRecords = Main.getErroneousRecords(rawBids)
+    //expected.foreach(x=>println(x))
+   // erroneousRecords.foreach(x=>println(x))
+
+    assertRDDEquals(expected, erroneousRecords)
+  }
+
+
+  test("should filter errors and create correct aggregates") {
+
+    runIntegrationTest()
+
+      //If the test fails and you are interested in what are the differences in the RDDs uncomment the corresponding line
+    printRddDifferences(EXPECTED_ERRORS_INTEGRATION, getOutputPath(ERRONEOUS_DIR))
+    printRddDifferences(EXPECTED_AGGREGATED_INTEGRATION, getOutputPath(AGGREGATED_DIR))
+
+    assertRddTextFiles(EXPECTED_ERRORS_INTEGRATION, getOutputPath(ERRONEOUS_DIR))
+    assertAggregatedFiles(EXPECTED_AGGREGATED_INTEGRATION, getOutputPath(AGGREGATED_DIR))
+  }
+
+  after {
+    outputFolder.delete
+  }
+
+  private def runIntegrationTest() = {
+    Main.processData(sc, INPUT_BIDS_INTEGRATION, INPUT_MOTELS_INTEGRATION, INPUT_EXCHANGE_RATES_INTEGRATION, outputFolder.getAbsolutePath)
+  }
+
+  private def assertRddTextFiles(expectedPath: String, actualPath: String) = {
+    val expected = sc.textFile(expectedPath)
+    val actual = sc.textFile(actualPath)
+    assertRDDEquals(expected, actual)
+  }
+
+  private def assertAggregatedFiles(expectedPath: String, actualPath: String) = {
+    val expected = parseLastDouble(sc.textFile(expectedPath)).collect.toMap
+    val actual = parseLastDouble(sc.textFile(actualPath)).collect.toMap
+    if (expected.size != actual.size) {
+      Assert.fail(s"Aggregated have wrong number of records (${actual.size} instead of ${expected.size})")
+    }
+    expected.foreach(x => {
+      val key = x._1
+      val expectedValue = x._2
+      if (!actual.contains(key)) {
+        Assert.fail(s"Aggregated does not contain: $key,$expectedValue")
+      }
+      val actualValue = actual(key)
+      if (Math.abs(expectedValue - actualValue) > 0.0011) {
+        Assert.fail(s"Aggregated have different value for: $key ($actualValue instead of $expectedValue)")
+      }
+    })
+  }
+
+  private def parseLastDouble(rdd: RDD[String]) = {
+    rdd.map(s => {
+      val commaIndex = s.lastIndexOf(",")
+      (s.substring(0, commaIndex), s.substring(commaIndex + 1).toDouble)
+    })
+  }
+
+  private def printRddDifferences(expectedPath: String, actualPath: String) = {
+    val expected = sc.textFile(expectedPath)
+    val actual = sc.textFile(actualPath)
+    RddComparator.printDiff(expected, actual)
+  }
+
+  private def getOutputPath(dir: String): String = {
+    new Path(outputFolder.getAbsolutePath, dir).toString
+  }
+}
